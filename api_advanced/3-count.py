d13d67aad768025e781b6bc779fed6e8c37190de
sjamillah <j.ssozi@alustudent.com>
2023-11-10 10:51:27 +0000

    removed some bugs

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -21,7 +21,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
-                           headers={'user-agent': 'Mozilla/5.0 \(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'})
+                           headers={'user-agent': 'bhalut'})
 
     if request.status_code == 200:
         data = request.json()
